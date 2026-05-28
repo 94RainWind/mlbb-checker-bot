f2261dbd88566df0cdd3ee51887545331cfba310
94RainWind <soenaung92@gmail.com>
2026-05-28 14:45:25 +0630

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import telebot
 import requests
 
 # ⚠️ မိမိရဲ့ Telegram Bot Token ကို ဒီမှာ အစားထိုးပါ
-BOT_TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN_HERE'
+BOT_TOKEN = '8847386138:AAFAIbtJIFtAvrocKPi7vRDqIWDBti0RX2o'
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
